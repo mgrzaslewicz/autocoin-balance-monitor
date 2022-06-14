41a3646a143aba84047d22fe351f5c62a776c001
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2022-06-14 15:29:35 +0200

    Fix connection leaking in DB health check and close it after health check

--- src/main/kotlin/autocoin/balance/health/db/DbHealthCheck.kt
+++ src/main/kotlin/autocoin/balance/health/db/DbHealthCheck.kt
@@ -2,17 +2,23 @@ package autocoin.balance.health.db
 
 import autocoin.balance.health.HealthCheck
 import autocoin.balance.health.HealthCheckResult
+import com.zaxxer.hikari.HikariDataSource
 import mu.KLogging
+import java.sql.Connection
+import java.sql.PreparedStatement
 import java.util.concurrent.atomic.AtomicReference
-import javax.sql.DataSource
 
-class DbHealthCheck(private val datasource: AtomicReference<DataSource>) : HealthCheck {
+class DbHealthCheck(private val datasource: AtomicReference<HikariDataSource>) : HealthCheck {
     private companion object : KLogging()
 
     private fun getDbConnectionStatus(): Pair<Boolean, String?> {
+        var preparedStatement: PreparedStatement? = null
+        var connection: Connection? = null
         return try {
             if (datasource.get() != null) {
-                datasource.get().connection.prepareStatement("select 1").execute()
+                connection = datasource.get().connection
+                preparedStatement = connection.prepareStatement("select 1")
+                preparedStatement.execute()
                 Pair(true, null)
             } else {
                 Pair(false, "Datasource not created yet")
@@ -20,6 +26,9 @@ class DbHealthCheck(private val datasource: AtomicReference<DataSource>) : Healt
         } catch (e: Exception) {
             logger.error(e) { "DB connection issue" }
             Pair(false, e.message)
+        } finally {
+            preparedStatement?.close()
+            connection?.close()
         }
     }
 
@@ -29,6 +38,11 @@ class DbHealthCheck(private val datasource: AtomicReference<DataSource>) : Healt
         return HealthCheckResult(
             description = "PostgreSQL connection",
             healthy = dbConnectionStatus.first,
+            details = mapOf(
+                "activeConnections" to (datasource.get()?.hikariPoolMXBean?.activeConnections?.toString() ?: "0"),
+                "idleConnections" to (datasource.get()?.hikariPoolMXBean?.idleConnections?.toString() ?: "0"),
+                "totalConnections" to (datasource.get()?.hikariPoolMXBean?.totalConnections?.toString() ?: "0"),
+            ),
             unhealthyReasons = listOfNotNull(
                 if (!dbConnectionStatus.first) "DB connection issue, reason: ${dbConnectionStatus.second}" else null
             ),
