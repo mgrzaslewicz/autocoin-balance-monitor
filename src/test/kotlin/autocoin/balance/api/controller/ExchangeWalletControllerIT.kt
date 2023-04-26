2d852d5193f251e87d5980e9d7796f5930baab17
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-04-26 19:59:58 +0200

    Add ExchangeWalletControllerIT

---
+++ src/test/kotlin/autocoin/balance/api/controller/ExchangeWalletControllerIT.kt
@@ -0,0 +1,44 @@
+package autocoin.balance.api.controller
+
+import autocoin.TestServer.Companion.startTestServer
+import autocoin.balance.app.ObjectMapperProvider
+import autocoin.balance.wallet.exchange.ExchangeWalletBalancesDto
+import automate.profit.autocoin.exchange.time.SystemTimeMillisProvider
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+
+class ExchangeWalletControllerIT {
+
+    private val httpClientWithoutAuthorization = OkHttpClient()
+    private val objectMapper = ObjectMapperProvider().createObjectMapper()
+    private val authenticatedHttpHandlerWrapper = AuthenticatedHttpHandlerWrapper()
+
+    @Test
+    fun shouldGetExchangeWallets() {
+        // given
+        val tested = ExchangeWalletController(
+            objectMapper = objectMapper,
+            oauth2BearerTokenAuthHandlerWrapper = authenticatedHttpHandlerWrapper,
+            userExchangeWalletService = mock(),
+            userExchangeWalletRepository = { mock() },
+            isUserInProPlanFunction = { false },
+            priceService = mock(),
+            timeMillisProvider = SystemTimeMillisProvider(),
+        )
+        val startedServer = startTestServer(tested)
+        val request = Request.Builder().url("http://localhost:${startedServer.port}/exchange/wallets")
+            .build()
+        // when
+        val response = httpClientWithoutAuthorization.newCall(request).execute()
+        // then
+        response.use {
+            assertThat(response.isSuccessful).isTrue
+            val responseDto = objectMapper.readValue(it.body?.string(), ExchangeWalletBalancesDto::class.java)
+            assertThat(responseDto.exchangeCurrencyBalances).isNotEmpty
+        }
+        startedServer.stop()
+    }
+}
