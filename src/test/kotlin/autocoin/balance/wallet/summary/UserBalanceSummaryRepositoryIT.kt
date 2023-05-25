df557881c2e98c412e4d9464058a0ebc5d64d159
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2023-05-25 17:43:59 +0200

    Improve UseBalanceSummaryRepositoryIT speed by reusing postgresql container

--- src/test/kotlin/autocoin/balance/wallet/summary/UserBalanceSummaryRepositoryIT.kt
+++ src/test/kotlin/autocoin/balance/wallet/summary/UserBalanceSummaryRepositoryIT.kt
@@ -7,28 +7,50 @@ import autocoin.balance.wallet.exchange.UserExchangeWallet
 import autocoin.balance.wallet.exchange.UserExchangeWalletRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.jdbi.v3.core.Jdbi
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import java.math.BigDecimal
 import java.util.*
 
 class UserBalanceSummaryRepositoryIT {
-    private lateinit var startedDatabase: TestDb.StartedDatabase
     private lateinit var jdbi: Jdbi
     private lateinit var tested: UserBalanceSummaryRepository
     private lateinit var exchangeWalletRepository: UserExchangeWalletRepository
     private lateinit var blockchainWalletRepository: UserBlockChainWalletRepository
 
     private val doesNotMatter = BigDecimal.ONE
-   private val userAccountId = UUID.randomUUID().toString()
+    private val userAccountId = UUID.randomUUID().toString()
+
+    companion object {
+        private lateinit var startedDatabase: TestDb.StartedDatabase
+
+        @BeforeAll
+        @JvmStatic
+        fun startDb() {
+            startedDatabase = TestDb.startDatabase()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun stopDb() {
+            startedDatabase.container.stop()
+        }
+    }
+
 
     @BeforeEach
     fun setup() {
-        startedDatabase = TestDb.startDatabase()
         jdbi = startedDatabase.jdbi
+        startedDatabase.runMigrations()
         tested = jdbi.onDemand(UserBalanceSummaryRepository::class.java)
         exchangeWalletRepository = jdbi.onDemand(UserExchangeWalletRepository::class.java)
         blockchainWalletRepository = jdbi.onDemand(UserBlockChainWalletRepository::class.java)
+    }
+
+    @AfterEach
+    fun cleanup() {
+        jdbi.useHandle<Exception> { handle ->
+            handle.execute("""select 'drop table "' || tablename || '" cascade;' from pg_tables;""")
+        }
     }
 
     @Test
@@ -74,10 +96,22 @@ class UserBalanceSummaryRepositoryIT {
             )
         )
         blockchainWalletRepository.insertWallet(
-            UserBlockChainWallet(userAccountId = userAccountId, walletAddress = "wallet-1", currency = "BTC", balance = null, description = null)
+            UserBlockChainWallet(
+                userAccountId = userAccountId,
+                walletAddress = "wallet-1",
+                currency = "BTC",
+                balance = null,
+                description = null
+            )
         )
         blockchainWalletRepository.insertWallet(
-            UserBlockChainWallet(userAccountId = userAccountId, walletAddress = "wallet-2", currency = "ABC", balance = null, description = null)
+            UserBlockChainWallet(
+                userAccountId = userAccountId,
+                walletAddress = "wallet-2",
+                currency = "ABC",
+                balance = null,
+                description = null
+            )
         )
     }
 
