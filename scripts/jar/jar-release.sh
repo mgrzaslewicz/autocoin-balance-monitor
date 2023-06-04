27abc547443a20694bb88c0184dac1a4a27c8360
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2023-06-04 22:19:29 +0200

    Fix jar-release so it pushes git commits when done

--- scripts/jar/jar-release.sh
+++ scripts/jar/jar-release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # turn on debug mode
 set -x
+# exit on any error
+set -e
 
-rm pom.xml.releaseBackup
-rm release.properties
+rm -f pom.xml.releaseBackup
+rm -f release.properties
 mvn clean release:prepare git-commit-id:revision
+git push
