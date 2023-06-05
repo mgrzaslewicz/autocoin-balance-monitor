aca2ef155da50986fdc7d3e981808d264ece4848
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2023-06-05 16:14:33 +0200

    Fail fast in Server when duplicated endpoint urls provided

--- src/main/kotlin/autocoin/balance/api/ServerBuilder.kt
+++ src/main/kotlin/autocoin/balance/api/ServerBuilder.kt
@@ -14,11 +14,12 @@ class ServerBuilder(
 ) {
     fun build(): Undertow {
         val routingHandler = RoutingHandler()
-        apiControllers.forEach {
-            it.apiEndpoints().forEach { handler ->
-                routingHandler.add(handler.method, handler.urlTemplate, handler.httpHandler)
-            }
+        val apiEndpoints = apiControllers.flatMap { it.apiEndpoints() }
+        verifyNoDuplicateEndpoints(apiEndpoints)
+        apiEndpoints.forEach { handler ->
+            routingHandler.add(handler.method, handler.urlTemplate, handler.httpHandler)
         }
+
         return Undertow.builder()
             .addHttpListener(appServerPort, "0.0.0.0")
             .setHandler(
@@ -31,12 +32,23 @@ class ServerBuilder(
             .build()
     }
 
+    private fun verifyNoDuplicateEndpoints(apiEndpoints: List<ApiEndpoint>) {
+        apiEndpoints.groupBy { "${it.urlTemplate}:${it.method}" to it.urlTemplate }
+            .filter { it.value.size > 1 }
+            .forEach {
+                throw IllegalArgumentException("Duplicated endpoint implementations: ${it.value.first().urlTemplate}:${it.value.first().method} ${it.value}")
+            }
+    }
+
     private fun HttpHandler.wrapWithCorsHeadersHandler(): HttpHandler {
         return HttpHandler {
             with(it.responseHeaders) {
                 put(tryFromString("Access-Control-Allow-Origin"), "*")
                 put(tryFromString("Access-Control-Allow-Credentials"), "true")
-                put(tryFromString("Access-Control-Allow-Headers"), "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control")
+                put(
+                    tryFromString("Access-Control-Allow-Headers"),
+                    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control"
+                )
                 put(tryFromString("Access-Control-Allow-Methods"), "GET, HEAD, POST, PUT, DELETE, OPTIONS")
                 put(tryFromString("Access-Control-Max-Age"), "3600")
             }
